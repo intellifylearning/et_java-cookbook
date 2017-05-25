881802c0139e223085e464d4293cb130686402b8
Eric Herot <eric.github@herot.com>
2017-05-25 17:21:50 -0400

    Version bump 1.50.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This file is used to list changes made in each version of et_java.
 
+## v1.50.0 (2017-05-25)
+
+* Repurpose this cookbook to override the Oracle Java download URL
+
 ## v0.1.0 (2013-07-22)
 
 * Initial release of et_java
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'All rights reserved'
 description      'EverTrue wrapper cookbook for the Java cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.50.0'
 
 depends 'java', '~> 1.50.0'
