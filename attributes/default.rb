fc4be6975d2011ee039914144342dc9ce6792cec
Jared Stehler <jared.stehler@gmail.com>
2018-04-03 21:13:18 -0400

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
 override['java']['jdk']['8']['x86_64']['url'] =
-  'http://ops.evertrue.com.s3.amazonaws.com/pkgs/jdk-8u131-linux-x64.tar.gz'
+  'https://intellifydev.s3.amazonaws.com/public/jdk-8u152-linux-x64.tar.gz'
